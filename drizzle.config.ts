118b8a4fc54e1991cc0001c369303cc287786826
liuw <liuw5367@gmail.com>
2026-05-22 11:39:06 +0800

    fix: url

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,4 @@
+import process from 'node:process'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
@@ -5,7 +6,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    // url: process.env.DATABASE_URL!,
-    url: 'postgresql://postgres.xvumdxzysnfmyilysinn:Aj1FkdZE6MTYtvKa@aws-1-ap-northeast-1.pooler.supabase.com:6543/postgres',
+    url: process.env.DATABASE_URL!,
   },
 })
